eac0bec081bbe31811c4b7410f510e83f16f15f6
Abhey Sharma <abheysharma@Abheys-MacBook-Air.local>
2024-01-11 17:51:49 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ElectronService } from 'ngx-electronyzer';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gulmondatest2';
+  constructor(private electronService: ElectronService){
+
+  }
+
+  openExternalApp() {
+    console.log('openExternalApp');
+    console.log(this.electronService.isMacOS)
+    this.electronService.shell.openExternal('https://example.com', { activate: true });
+  }
+
+  openItem() {
+    console.log('openItem');
+    if (this.electronService.shell) {
+      console.log('hello')
+      this.electronService.shell.openPath('/Users/username/Desktop/file.txt');
+    }
+    
+  }
+
+  
+  
 }
+
+
